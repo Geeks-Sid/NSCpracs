f4f606dd52cd97902feef909b59d5bb6bd9b4eb9
Geeks-Sid <sid.cre8er@gmail.com>
2017-03-22 12:23:07 +0530

    Add files via upload

--- newtoninter.c
+++ newtoninter.c
@@ -12,7 +12,7 @@ void init() {
 			mat[i][j] = 0;
 }
 
-void matprinter() {
+void matprinter() {/*                             //Please remove comments to print the DD matrix everytime
 	printf("\n\n\n");
 	int i,j;
 	for (i = 0; i < number; i++) {
@@ -21,17 +21,17 @@ void matprinter() {
 				]);
 		}
 		printf("\n");
-	}
+	}*/
 }
 
 void ddcalc() {
 	int i,j;
 	for (i = 1; i < number; i++) {
 		for (j = 0; j < number-i; j++) {
-			mat[j][i] = mat[j+1][i-1] - mat[j][i-1];
+			mat[j][i] = mat[j+1][i-1] - mat[j][i-1 ];
 		}
 	}
-	printf("\nInside ddcalc");
+//	printf("\nInside ddcalc");
 	matprinter();
 }
 
@@ -42,10 +42,35 @@ void matfirst() {
 	{
 		mat[i][0] = fx[i];
 	}
-	printf("\nInside Mat first");
+	//printf("\nInside Mat first");
 	matprinter();
 }
 
+void polyprint() { //Polyprint
+	int i,j;
+	for (i = 0; i < number; i++) {
+		printf("%lf*",mat[0][i]);
+		for(j = 0;j<i;j++) {
+			printf("(x-%lf)",x[j]);
+		}
+		printf(" + ");
+	}
+}
+
+void solver(double val) {
+	int i,j;
+	double temp2 = 1,temp3 = 0;
+	for (i = 0 ;i< number;i++) {
+		temp = mat[0][i];
+		temp2 = 1;
+		for(j = 0;j<i;j++) {
+			temp2 = temp2 * (val - x[j])
+		}
+		temp = temp*temp2;
+		temp3 += temp;
+	}
+	printf("The value of Polynomial at %lf is:%lf",val,temp3);
+}
 void main() {
 	
 	int i;
@@ -66,4 +91,6 @@ void main() {
 	matprinter();
 	matfirst();
 	ddcalc();
+	polyprint();
 }
+
