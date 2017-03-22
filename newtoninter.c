8344cbe43e10b6b8ea1c1eedb6e4d98d47afb7aa
Geeks-Sid <sid.cre8er@gmail.com>
2017-03-22 11:18:49 +0530

    Add files via upload

---
+++ newtoninter.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<math.h>
+double mat[10][10];
+double x[10];
+double fx[10];
+int number;
+
+void init() {
+	int i,j;
+	for (i = 1; i < number; i++) 
+		for (j = 0; j < number; j++) 
+			mat[i][j] = 0;
+}
+
+void matprinter() {
+	printf("\n\n\n");
+	int i,j;
+	for (i = 0; i < number; i++) {
+		for (j = 0; j < number; j++) {
+			printf("\t %lf",mat[i][j
+				]);
+		}
+		printf("\n");
+	}
+}
+
+void ddcalc() {
+	int i,j;
+	for (i = 1; i < number; i++) {
+		for (j = 0; j < number-i; j++) {
+			mat[j][i] = mat[j+1][i-1] - mat[j][i-1];
+		}
+	}
+	printf("\nInside ddcalc");
+	matprinter();
+}
+
+
+void matfirst() {
+	int i;
+	for (i = 0; i < number; i++)
+	{
+		mat[i][0] = fx[i];
+	}
+	printf("\nInside Mat first");
+	matprinter();
+}
+
+void main() {
+	
+	int i;
+	
+	printf("\nPlease Enter the number of samples you are going to give(<10):");
+	scanf("%d",&number);	
+	
+	printf("\nPlease Enter the values of X first :");
+	for (i = 0; i < number; i++)
+		scanf("%lf",&x[i]);
+	
+	printf("\nPlease Enter the values of F(X) now :");
+	for (i = 0; i < number; i++) {
+		printf("\nF(%lf) = ",x[i]);
+		scanf("%lf",&fx[i]);
+	}
+	init();
+	matprinter();
+	matfirst();
+	ddcalc();
+}
